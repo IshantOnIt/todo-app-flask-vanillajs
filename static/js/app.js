38b8285a10bf8f90f775725202cb9bd322acbf10
Ishant <223127347+IshantOnIt@users.noreply.github.com>
2025-07-30 19:27:54 +0530

    add logic to display and toggle task status

--- static/js/app.js
+++ static/js/app.js
@@ -8,13 +8,46 @@ document.addEventListener("DOMContentLoaded", function () {
       const res = await fetch("/api/todos");
       const todos = await res.json();
       todos.forEach((todo) => {
-        const listItem = document.createElement("li");
-        listItem.innerText = todo.title;
-        todoList.appendChild(listItem);
+        createTodo(todo);
       });
     } catch (e) {
       console.error("Error loading todos: ", e);
     }
+  }
+
+  function createTodo(todo) {
+    const listItemCheckbox = document.createElement("input");
+    listItemCheckbox.type = "checkbox";
+    listItemCheckbox.checked = todo.completed;
+    listItemCheckbox.addEventListener("change", async (e) => {
+      try {
+        const res = await fetch(`/api/todos/${todo.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ completed: e.target.checked }),
+        });
+        const updatedTodo = await res.json();
+        if (!updatedTodo) {
+          throw new Error("Error updating todo");
+        }
+        listItemCheckbox.checked = updatedTodo.completed;
+      } catch (e) {
+        console.error("Error updating todo: ", e);
+      }
+    });
+
+    const listItemSpan = document.createElement("span");
+    listItemSpan.innerText = todo.title;
+
+    const listItemLabel = document.createElement("label");
+    listItemLabel.appendChild(listItemCheckbox);
+    listItemLabel.appendChild(listItemSpan);
+
+    const listItem = document.createElement("li");
+    listItem.appendChild(listItemLabel);
+    todoList.appendChild(listItem);
   }
 
   todoform.addEventListener("submit", async (e) => {
@@ -30,9 +63,12 @@ document.addEventListener("DOMContentLoaded", function () {
       });
       const newTodo = await res.json();
 
-      const listItem = document.createElement("li");
-      listItem.innerText = newTodo.title;
-      todoList.appendChild(listItem);
+      if (!newTodo) {
+        throw new Error("Error creating new todo");
+      }
+
+      createTodo(newTodo);
+
       todoTitle.value = "";
     } catch (e) {
       console.error("Error creating new todo: ", e);
